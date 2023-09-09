0529bd007052543c25e48391c457e51d34e249e0
Stav Cohen <stavco28@gmail.com>
2023-09-09 13:01:50 +0300

    note: add done/not done style for list

--- apps/note/cmps/NoteTodos.jsx
+++ apps/note/cmps/NoteTodos.jsx
@@ -1,16 +1,29 @@
+const { useState } = React
+
 export function NoteTodos({ note }) {
-    let count = 0
     const { info, title } = note
-    const toDos = info.todos
+    const { todos } = info
+    const [todosState, setTodosState] = useState(todos)
+
+    function handleClickTodo(index) {
+        const updatedTodos = [...todosState]
+        updatedTodos[index].done = !updatedTodos[index].done
+        setTodosState(updatedTodos)
+    }
 
     return (
-        <React.Fragment>
+        <div>
             <p>{title}</p>
-            <ul>
-                {toDos.map((todo) => (
-                    <li key={count++}>{todo.txt}</li>
+            <ul className="todo-list-card">
+                {todosState.map((todo, index) => (
+                    <li
+                        className={`todo-line ${todo.done ? 'line-through' : ''}`}
+                        key={index}
+                        onClick={() => handleClickTodo(index)}>
+                        {todo.txt}
+                    </li>
                 ))}
             </ul>
-        </React.Fragment>
+        </div>
     )
 }
