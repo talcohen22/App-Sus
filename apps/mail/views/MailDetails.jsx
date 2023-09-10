97767785ca040f28cf5dc981b0bb24591b934401
Stav Cohen <stavco28@gmail.com>
2023-09-10 23:36:52 +0300

    fix img2

--- apps/mail/views/MailDetails.jsx
+++ apps/mail/views/MailDetails.jsx
@@ -9,14 +9,14 @@ export function MailDetails() {
     const navigate = useNavigate()
 
     useEffect(() => {
-        mailService.get(params.emailId).then(email => {
+        mailService.get(params.emailId).then((email) => {
             email.isRead = true
             mailService.put(email).then(setEmail)
         })
     }, [])
 
     function markAsUnRead() {
-        mailService.get(params.emailId).then(email => {
+        mailService.get(params.emailId).then((email) => {
             email.isRead = false
             mailService.put(email).then(() => {
                 onBack()
@@ -31,13 +31,15 @@ export function MailDetails() {
     if (!email) return
     return (
         <div className="mail-details-bgc">
-
             <div className="mail-details-container">
-                <div className='actions back-narrow'>
-                    <i onClick={() => onBack()} className="fa-solid fa-arrow-left-long back-btn" ></i>
+                <div className="actions back-narrow">
+                    <i onClick={() => onBack()} className="fa-solid fa-arrow-left-long back-btn"></i>
                 </div>
-                <div className='actions'>
-                    <i className="fa-regular fa-envelope" onClick={() => markAsUnRead()} title="Mark as unread"></i>
+                <div className="actions">
+                    <i
+                        className="fa-regular fa-envelope"
+                        onClick={() => markAsUnRead()}
+                        title="Mark as unread"></i>
                     <i className="fa-solid fa-trash-can"></i>
                     <i className="fa-solid fa-tag"></i>
                     <i className="fa-regular fa-star"></i>
@@ -47,21 +49,24 @@ export function MailDetails() {
 
                 <h1>{email.subject}</h1>
 
-                <div className='sent-by user-img'>
-                    <img src="../../../assets/img/mail/user.png" alt="" />
+                <div className="sent-by user-img">
+                    <img src="./././assets/img/mail/user.png" alt="" />
                 </div>
-                <div className='sent-by'>
-                    <p>{"<" + email.from + ">"}</p>
+                <div className="sent-by">
+                    <p>{'<' + email.from + '>'}</p>
                 </div>
-                <p className='sent-to'>{"to " + email.to}</p>
+                <p className="sent-to">{'to ' + email.to}</p>
 
-                <p className='mail-body'>{email.body}</p>
+                <p className="mail-body">{email.body}</p>
 
-                <div className='replay-forward-btn'>
-                    <button><i className="fa-solid fa-reply"></i>Replay</button>
-                    <button><i className="fa-solid fa-reply fa-flip-horizontal"></i>Forward</button>
+                <div className="replay-forward-btn">
+                    <button>
+                        <i className="fa-solid fa-reply"></i>Replay
+                    </button>
+                    <button>
+                        <i className="fa-solid fa-reply fa-flip-horizontal"></i>Forward
+                    </button>
                 </div>
-
             </div>
         </div>
     )
