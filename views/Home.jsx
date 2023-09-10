93081bee16c893b8ca52c6f2196b36d80a01b505
Stav Cohen <stavco28@gmail.com>
2023-09-10 08:34:05 +0300

    app: fix img

--- views/Home.jsx
+++ views/Home.jsx
@@ -2,7 +2,12 @@
 export function Home() {
     return (
         <section className="home">
-            <img className="home-img" src="../assets/img/app/Google-Home-Logo.png" alt="" />
+            {/* <img className="home-img" src="../assets/img/app/Google-Home-Logo.png" alt="" /> */}
+            <img
+                className="home-img"
+                src="https://logos-world.net/wp-content/uploads/2021/08/Google-Home-Logo.png"
+                alt=""
+            />
         </section>
     )
 }
